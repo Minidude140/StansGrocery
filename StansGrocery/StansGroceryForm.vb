a3857ddc79572f87995d93afc0c14caf291e5449
Zac <zac.minidude140@Gmail.com>
2023-12-06 12:01:41 -0700

    Added Set Defaults Sub on form load

--- StansGrocery/StansGroceryForm.vb
+++ StansGrocery/StansGroceryForm.vb
@@ -9,5 +9,20 @@ Option Explicit On
 
 
 Public Class StansGroceryForm
+    'Custom Methods
+
+    ''' <summary>
+    ''' Sets the default Filters and Texts
+    ''' </summary>
+    Sub SetDefaults()
+        FilterByAisleRadioButton.Checked = True
+        DisplayLabel.Text = "No Item Selected."
+        FilterComboBox.SelectedIndex = 0
+    End Sub
+
+    'Event Handlers
+    Private Sub StansGroceryForm_Load(sender As Object, e As EventArgs) Handles Me.Load
+        SetDefaults()
+    End Sub
 
 End Class
