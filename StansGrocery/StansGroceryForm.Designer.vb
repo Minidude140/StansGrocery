ad678cfea99738d7697c69c5dd2f0ca3078ac3df
Zac <zac.minidude140@Gmail.com>
2023-12-06 11:50:23 -0700

    Added Controls to form

--- StansGrocery/StansGroceryForm.Designer.vb
+++ StansGrocery/StansGroceryForm.Designer.vb
@@ -22,10 +22,135 @@ Partial Class StansGroceryForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.SeachTextBox = New System.Windows.Forms.TextBox()
+        Me.SearchButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.FilterGroupBox = New System.Windows.Forms.GroupBox()
+        Me.FilterByAisleRadioButton = New System.Windows.Forms.RadioButton()
+        Me.FilterByCategoryRadioButton = New System.Windows.Forms.RadioButton()
+        Me.DisplayListBox = New System.Windows.Forms.ListBox()
+        Me.DisplayLabel = New System.Windows.Forms.Label()
+        Me.FilterComboBox = New System.Windows.Forms.ComboBox()
+        Me.FilterGroupBox.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'SeachTextBox
+        '
+        Me.SeachTextBox.Location = New System.Drawing.Point(15, 61)
+        Me.SeachTextBox.Name = "SeachTextBox"
+        Me.SeachTextBox.Size = New System.Drawing.Size(326, 22)
+        Me.SeachTextBox.TabIndex = 0
+        '
+        'SearchButton
+        '
+        Me.SearchButton.Location = New System.Drawing.Point(347, 57)
+        Me.SearchButton.Name = "SearchButton"
+        Me.SearchButton.Size = New System.Drawing.Size(105, 30)
+        Me.SearchButton.TabIndex = 1
+        Me.SearchButton.Text = "&Search"
+        Me.SearchButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.ExitButton.Location = New System.Drawing.Point(665, 398)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(123, 40)
+        Me.ExitButton.TabIndex = 2
+        Me.ExitButton.Text = "E&xit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'FilterGroupBox
+        '
+        Me.FilterGroupBox.Controls.Add(Me.FilterByCategoryRadioButton)
+        Me.FilterGroupBox.Controls.Add(Me.FilterByAisleRadioButton)
+        Me.FilterGroupBox.Location = New System.Drawing.Point(12, 97)
+        Me.FilterGroupBox.Name = "FilterGroupBox"
+        Me.FilterGroupBox.Size = New System.Drawing.Size(328, 52)
+        Me.FilterGroupBox.TabIndex = 3
+        Me.FilterGroupBox.TabStop = False
+        Me.FilterGroupBox.Text = "Filter"
+        '
+        'FilterByAisleRadioButton
+        '
+        Me.FilterByAisleRadioButton.AutoSize = True
+        Me.FilterByAisleRadioButton.Location = New System.Drawing.Point(50, 19)
+        Me.FilterByAisleRadioButton.Name = "FilterByAisleRadioButton"
+        Me.FilterByAisleRadioButton.Size = New System.Drawing.Size(79, 21)
+        Me.FilterByAisleRadioButton.TabIndex = 0
+        Me.FilterByAisleRadioButton.TabStop = True
+        Me.FilterByAisleRadioButton.Text = "By Aisle"
+        Me.FilterByAisleRadioButton.UseVisualStyleBackColor = True
+        '
+        'FilterByCategoryRadioButton
+        '
+        Me.FilterByCategoryRadioButton.AutoSize = True
+        Me.FilterByCategoryRadioButton.Location = New System.Drawing.Point(171, 19)
+        Me.FilterByCategoryRadioButton.Name = "FilterByCategoryRadioButton"
+        Me.FilterByCategoryRadioButton.Size = New System.Drawing.Size(106, 21)
+        Me.FilterByCategoryRadioButton.TabIndex = 1
+        Me.FilterByCategoryRadioButton.TabStop = True
+        Me.FilterByCategoryRadioButton.Text = "By Category"
+        Me.FilterByCategoryRadioButton.UseVisualStyleBackColor = True
+        '
+        'DisplayListBox
+        '
+        Me.DisplayListBox.FormattingEnabled = True
+        Me.DisplayListBox.ItemHeight = 16
+        Me.DisplayListBox.Location = New System.Drawing.Point(498, 56)
+        Me.DisplayListBox.Name = "DisplayListBox"
+        Me.DisplayListBox.Size = New System.Drawing.Size(273, 292)
+        Me.DisplayListBox.TabIndex = 4
+        '
+        'DisplayLabel
+        '
+        Me.DisplayLabel.Location = New System.Drawing.Point(59, 380)
+        Me.DisplayLabel.Name = "DisplayLabel"
+        Me.DisplayLabel.Size = New System.Drawing.Size(560, 36)
+        Me.DisplayLabel.TabIndex = 5
+        Me.DisplayLabel.Text = "Show text for selected Display list box item"
+        Me.DisplayLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'FilterComboBox
+        '
+        Me.FilterComboBox.FormattingEnabled = True
+        Me.FilterComboBox.Location = New System.Drawing.Point(17, 194)
+        Me.FilterComboBox.Name = "FilterComboBox"
+        Me.FilterComboBox.Size = New System.Drawing.Size(434, 24)
+        Me.FilterComboBox.TabIndex = 6
+        Me.FilterComboBox.Text = "Show All"
+        '
+        'StansGroceryForm
+        '
+        Me.AcceptButton = Me.SearchButton
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.ExitButton
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.Controls.Add(Me.FilterComboBox)
+        Me.Controls.Add(Me.DisplayLabel)
+        Me.Controls.Add(Me.DisplayListBox)
+        Me.Controls.Add(Me.FilterGroupBox)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.SearchButton)
+        Me.Controls.Add(Me.SeachTextBox)
+        Me.Name = "StansGroceryForm"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Stan's Grocery"
+        Me.FilterGroupBox.ResumeLayout(False)
+        Me.FilterGroupBox.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents SeachTextBox As TextBox
+    Friend WithEvents SearchButton As Button
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents FilterGroupBox As GroupBox
+    Friend WithEvents FilterByCategoryRadioButton As RadioButton
+    Friend WithEvents FilterByAisleRadioButton As RadioButton
+    Friend WithEvents DisplayListBox As ListBox
+    Friend WithEvents DisplayLabel As Label
+    Friend WithEvents FilterComboBox As ComboBox
 End Class
