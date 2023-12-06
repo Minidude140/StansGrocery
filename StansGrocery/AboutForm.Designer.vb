6d95fb28c0aac6d6ff6d9141712f3fcb1b8b8bc4
Zac <zac.minidude140@Gmail.com>
2023-12-06 12:12:12 -0700

    Added About Form

---
+++ StansGrocery/AboutForm.Designer.vb
@@ -0,0 +1,68 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class AboutForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(AboutForm))
+        Me.AboutCloseButton = New System.Windows.Forms.Button()
+        Me.AboutLabel = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'AboutCloseButton
+        '
+        Me.AboutCloseButton.Location = New System.Drawing.Point(193, 229)
+        Me.AboutCloseButton.Name = "AboutCloseButton"
+        Me.AboutCloseButton.Size = New System.Drawing.Size(150, 45)
+        Me.AboutCloseButton.TabIndex = 0
+        Me.AboutCloseButton.Text = "Close"
+        Me.AboutCloseButton.UseVisualStyleBackColor = True
+        '
+        'AboutLabel
+        '
+        Me.AboutLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.AboutLabel.Location = New System.Drawing.Point(28, 22)
+        Me.AboutLabel.Name = "AboutLabel"
+        Me.AboutLabel.Size = New System.Drawing.Size(475, 204)
+        Me.AboutLabel.TabIndex = 1
+        Me.AboutLabel.Text = resources.GetString("AboutLabel.Text")
+        Me.AboutLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'AboutForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(510, 284)
+        Me.Controls.Add(Me.AboutLabel)
+        Me.Controls.Add(Me.AboutCloseButton)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.MaximumSize = New System.Drawing.Size(528, 331)
+        Me.MinimumSize = New System.Drawing.Size(528, 331)
+        Me.Name = "AboutForm"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "About"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents AboutCloseButton As Button
+    Friend WithEvents AboutLabel As Label
+End Class
