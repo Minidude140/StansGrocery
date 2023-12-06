7769517e20eab93cb767289dacaa0dcbd00e7bcc
Zac <zac.minidude140@Gmail.com>
2023-12-06 13:43:13 -0700

    Added file input.  Needs formating

--- StansGrocery/StansGroceryForm.vb
+++ StansGrocery/StansGroceryForm.vb
@@ -9,6 +9,10 @@ Option Explicit On
 
 
 Public Class StansGroceryForm
+    Dim currentrecord() As String
+    Dim fileName As String = "..\..\Grocery.txt"
+    Dim testString As String
+
     'Custom Methods
 
     ''' <summary>
@@ -20,9 +24,26 @@ Public Class StansGroceryForm
         FilterComboBox.SelectedIndex = -1
     End Sub
 
+    Sub LoadInventoryFile()
+        Dim fileNumber As Integer = FreeFile()
+        Dim currentField As String = ""
+
+        Try
+            FileOpen(fileNumber, Me.fileName, OpenMode.Input)
+            Do Until EOF(fileNumber)
+                currentField = LineInput(fileNumber)
+                testString = testString & currentField
+            Loop
+            FileClose(fileNumber)
+        Catch ex As Exception
+
+        End Try
+    End Sub
     'Event Handlers
     Private Sub StansGroceryForm_Load(sender As Object, e As EventArgs) Handles Me.Load
         SetDefaults()
+        LoadInventoryFile()
+        DisplayLabel.Text = testString
     End Sub
 
     Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click,
